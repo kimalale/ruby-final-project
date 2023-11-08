02f9d6899556af322592ee7d689b95af389169da
Kim Malale <kimalale@hotmail.com>
2023-11-08 02:27:30 +0200

    Fix how_to_play method display

--- main.rb
+++ main.rb
@@ -1,23 +1,8 @@
 require './lib/chess.rb'
 
-choice = 0
-choice_list = [1, 2, 3]
-until choice_list.include?(choice) do
-  puts "Welcome to meChess!"
-  puts "1. Create a new game"
-  puts "2. Load saved game"
-  puts "3. See how to play"
-  print "Choice: "
-  choice = gets.chomp.to_i
-end
-
-system("clear") || system("cls")
-
-case choice
-when 1 then
-  board = Chess.new
-
+def play(board)
   until board.check_mate do
+    system("clear") || system("cls")
     puts "  meChess\n"
     puts "    Player: #{board.player.upcase}\n"
     #puts "Score: #{board.score}"
@@ -44,13 +29,36 @@ when 1 then
       exit
     end
 
+    next if initial == nil || destination == nil
     castle = nil if castle != "1"
     board.move_piece(initial, destination, castle)
-    system("clear") || system("cls")
   end
+end
+
+
+choice = 0
+choice_list = [1, 2, 3]
+until choice_list.include?(choice) do
+  puts "Welcome to meChess!"
+  puts "1. Create a new game"
+  puts "2. Load saved game"
+  puts "3. See how to play"
+  print "Choice: "
+  choice = gets.chomp.to_i
+end
+
+system("clear") || system("cls")
+
+case choice
+when 1 then
+  # Create a new board
+  board = Chess.new
+
+  play(board)
+
 when 2 then
-  puts " Enter filename to load:"
-  directory_path = './lib'
+  puts " Select filename to load:"
+  directory_path = './saved'
 
   # Get a list of all files in the specified directory
   files = Dir.entries(directory_path)
@@ -59,17 +67,23 @@ when 2 then
   json_files = files.select { |file| file.end_with?('.json') }
 
 # Print the list of JSON files
-  json_files.each do |json_file|
-    puts "1. #{json_file}"
+  json_files.each_with_index do |json_file, index|
+    puts "#{index}. #{json_file}"
   end
 
-  # select
+  # Select File
   choice = gets.chomp.to_i
-
   filename = json_files[choice]
-  load(filename, board)
+
+  # Load saved file to new Board !Continuation
+  board = Chess.new
+  load("#{directory_path}/" + filename, board)
+
+  # Resume
+  play(board)
 when 3 then
-  how_to_play
+  puts how_to_play # Guide
+  exit
 end
 
 
